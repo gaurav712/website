b6ba4be69e38d0347f4f40cf7f6c6685c35d4053
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2022-02-02 17:41:18 +0530

    about card skeleton

--- src/pages/Home/AboutCard.tsx
+++ src/pages/Home/AboutCard.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+//@ts-ignore
+import Styles from "../../stylesheets/AboutCard.module.css";
 
 const AboutCard: React.FC = () => {
-  return <></>;
+  return <div className={Styles.container}></div>;
 };
 
 export default AboutCard;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header";
+import AboutCard from "./AboutCard";
 
 const Home: React.FC = () => {
   return (
     <>
       <Header selectedTab="Home" />
+      <AboutCard />
     </>
   );
 };
