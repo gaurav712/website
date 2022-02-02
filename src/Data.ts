f489a1975d30fde7ead244c9e6810c22575425ec
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2022-02-03 02:11:48 +0530

    about card

---
+++ src/Data.ts
@@ -0,0 +1,20 @@
+const Greetings = [
+  ["Hello", "It's Gaurav"],
+  ["¡Hola!", "Soy Gaurav"],
+  ["Ciao!", "Io sono Gaurav"],
+  ["नमस्ते!", "मैं गौरव"],
+  ["Hallo!", "Ich bin Gaurav"],
+  ["Bonjour!", "Je m'appelle Gaurav"],
+  ["Hallo!", "Ik ben Gaurav"]
+];
+
+const Info = [
+  "A student, persuing B-Tech in Computer Science & Engineering.",
+  "Free software evangelist. I love FOSS projects.",
+  "Passionate about system-level programming and video games.",
+  "Loves exploring new technologies, ideas, anything science."
+];
+
+const ProjectList = [{}];
+
+export { Greetings, Info, ProjectList };
--- src/pages/Home/AboutCard.tsx
+++ src/pages/Home/AboutCard.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 //@ts-ignore
 import Styles from "../../stylesheets/AboutCard.module.css";
+import { Greetings, Info } from "../../Data";
+//@ts-ignore
+import Photo from "../../assets/me.png";
 
 const AboutCard: React.FC = () => {
-  return <div className={Styles.container}></div>;
+  const [greeting, setGreeting] = useState<Array<string>>(Greetings[0]);
+
+  useEffect(() => {
+    let count = 1;
+    setInterval(() => {
+      if (count === Greetings.length) {
+        count = 0;
+      }
+      setGreeting(Greetings[count]);
+      count = count + 1;
+    }, 3000);
+  }, []);
+
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.greet}>
+        <div className={Styles.greetMessage}>
+          <div>
+            {greeting[0]}{" "}
+            <span role="img" aria-label="">
+              👋
+            </span>
+          </div>
+          <div>{greeting[1]}</div>
+        </div>
+        <img id={Styles.photo} src={Photo} alt="" />
+      </div>
+      <div className={Styles.info}>
+        <ul>
+          {Info.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default AboutCard;
